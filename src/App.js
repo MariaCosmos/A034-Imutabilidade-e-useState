795ee979de95834bd6b5253ddc1ffda957c716ac
MariaCosmos <cosmos.maria2003@gmail.com>
2022-10-17 15:27:10 -0300

    Exercícios concluídos - Imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import styled, {createGlobalStyle} from 'styled-components'
 import PokemonCard from './Components/PokemonCard/PokemonCard';
+import { UseState } from 'react';
 
 const GlobalStyles = createGlobalStyle`
   * {
@@ -13,24 +14,45 @@ const FlexContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
 `
+
+
+
 function App() {
-  const [pokemon, setPokemon] = useState({
-    name: "Pichu",
-    type: "Electric",
-    evolved: false,
-    weight: 2,
-    color: 'yellow',
-    image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
+  const [pokemon3, setPokemon3] = useState({
+    nome:"Beedrill",
+    tipo:"Poison Bee",
+    evoluiu: false,
+    cor:"Amarelo",
+    peso:29.5,
+    img:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/015.png"
+  })
+  const [pokemon2, setPokemon2] = useState({
+    nome:"Pikachu",
+    tipo:"Eletrico",
+    evoluiu: true,
+    cor:"yellow",
+    peso:6,
+    img:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png",
+    id:0
+  })
+  const [pokemon1 , setPokemon1] = useState({
+    nome: "Pichu",
+    tipo: "Electrico",
+    evoluiu: false,
+    cor: "yellow",
+    peso: 2,
+    img: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
     id: 0
   })
 
   // Para fazer seus próximos pokemons, crie novos estados!
 
-  return ( <>
+  return ( 
+  <>
   <GlobalStyles/>
     <FlexContainer>
       {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
+      <PokemonCard cor={pokemon1.cor} nome={pokemon1.nome} tipo={pokemon1.tipo} peso={pokemon1.peso} img={pokemon1.img} setPokemon1={setPokemon1}/>
       {/* Crie aqui seus próximos pokemons! */}
     </FlexContainer>
   </>
